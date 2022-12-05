af8abbb59af8df966b36471bab6062cf10fa7513
Thomas Esimple <thomas.cipriani@esimple.it>
2022-12-05 17:55:27 +0100

    Parsing checkpoint

--- utils/parse.ts
+++ utils/parse.ts
@@ -1,5 +1,59 @@
 const input = (path = "input.txt") => new Input(Deno.readTextFileSync(path));
 
+const parse = (str: string) => new ParsingString(str);
+class ParsingString {
+  constructor(private content: string) {}
+
+  public getValue() {
+    return this.content;
+  }
+
+  public upper() {
+    this.content = this.content.toUpperCase();
+    return this;
+  }
+
+  public lower() {
+    this.content = this.content.toLowerCase();
+    return this;
+  }
+
+  public int() {
+    return new ParsingInt(parseInt(this.content));
+  }
+
+  public nlnl() {
+    const res = this.content.split("\n\n");
+    return new ParsingArray(res.map((r) => new ParsingString(r)));
+  }
+}
+
+class ParsingInt {
+  constructor(private content: number) {}
+
+  public getValue() {
+    return this.content;
+  }
+}
+
+parse("ahell").nlnl().getValue();
+
+type Parsings = ParsingString;
+
+class ParsingArray<T extends Parsings> {
+  constructor(private content: T[]) {}
+
+  public getValue() {
+    return this.content.map((i) => i.getValue());
+  }
+
+  public each(callbackFn: (value: T, index: number) => void) {
+    this.content.forEach(callbackFn);
+  }
+}
+
+const arr = [];
+
 class Input<T> {
   constructor(private content: T) {}
 
@@ -16,9 +70,10 @@ class Input<T> {
     }
   }
 }
-const parse = (str: string) => {};
 
 class Parser {
   public int() {}
 }
 export default parse;
+
+// parse('input').nlnl().int()
