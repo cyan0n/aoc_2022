5b7340a92053a2b863882545e4e3109f5c34c568
Thomas Esimple <thomas.cipriani@esimple.it>
2022-12-09 23:34:09 +0100

    Refactoring day 09

--- solutions/2022/09/b.ts
+++ solutions/2022/09/b.ts
@@ -12,56 +12,37 @@ const movements: Array<[Direction, number]> = input
       ],
   );
 const visited = new Set<string>(["0,0"]);
-const head: [number, number] = [0, 0];
-const trail: Array<[number, number]> = Array.from({ length: 9 }, () => [0, 0]);
+const rope: Array<[number, number]> = Array.from({ length: 10 }, () => [0, 0]);
+const head = rope[0];
 
 const follow = (leader: [number, number], follower: [number, number]) => {
-  if (leader[0] !== follower[0] || leader[1] !== follower[1]) {
-    const moveY = Math.abs(leader[0] - follower[0]) > 1;
-    const moveX = Math.abs(leader[1] - follower[1]) > 1;
-    if (moveY) {
-      follower[0] = (leader[0] + follower[0]) / 2;
-    } else if (moveX) {
-      follower[0] = leader[0];
-    }
-    if (moveX) {
-      follower[1] = (leader[1] + follower[1]) / 2;
-    } else if (moveY) {
-      follower[1] = leader[1];
-    }
+  const dY = Math.abs(leader[0] - follower[0]) > 1;
+  const dX = Math.abs(leader[1] - follower[1]) > 1;
+  if (dY) {
+    follower[0] = (leader[0] + follower[0]) / 2;
+  } else if (dX) {
+    follower[0] = leader[0];
+  }
+  if (dX) {
+    follower[1] = (leader[1] + follower[1]) / 2;
+  } else if (dY) {
+    follower[1] = leader[1];
   }
 };
 
-movements.forEach(([direction, distance]) => {
-  let steps = 0;
-  while (steps < distance) {
-    //move head
-    switch (direction) {
-      case "D":
-        head[0]--;
-        break;
-      case "U":
-        head[0]++;
-        break;
-      case "R":
-        head[1]++;
-        break;
-      case "L":
-        head[1]--;
-        break;
+movements.forEach(([direction, distance]) =>
+  Array.from({ length: distance }).forEach(() => {
+    if (direction === "D") {
+      head[0]--;
+    } else if (direction === "U") {
+      head[0]++;
+    } else if (direction === "L") {
+      head[1]--;
+    } else if (direction === "R") {
+      head[1]++;
     }
-    // console.log(head);
-    // tail
-
-    follow(head, trail[0]);
-    trail.forEach((t, i) => {
-      if (i > 0) {
-        follow(trail[i - 1], t);
-      }
-    });
-    steps++;
-
-    visited.add(trail[trail.length - 1].join());
-  }
-});
+    rope.forEach((t, i) => (i > 0 ? follow(rope[i - 1], t) : null));
+    visited.add(rope[rope.length - 1].join());
+  }),
+);
 console.log(visited.size);
