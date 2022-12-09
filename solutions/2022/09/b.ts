dd9aad517957a3dda968966ddd9617a3bbcc0720
Thomas Esimple <thomas.cipriani@esimple.it>
2022-12-09 23:34:09 +0100

    Cleaned logs

--- solutions/2022/09/b.ts
+++ solutions/2022/09/b.ts
@@ -17,16 +17,16 @@ const trail: Array<[number, number]> = Array.from({ length: 9 }, () => [0, 0]);
 
 const follow = (leader: [number, number], follower: [number, number]) => {
   if (leader[0] !== follower[0] || leader[1] !== follower[1]) {
-    const yDiff = Math.abs(leader[0] - follower[0]) > 1;
-    const xDiff = Math.abs(leader[1] - follower[1]) > 1;
-    if (yDiff) {
+    const moveY = Math.abs(leader[0] - follower[0]) > 1;
+    const moveX = Math.abs(leader[1] - follower[1]) > 1;
+    if (moveY) {
       follower[0] = (leader[0] + follower[0]) / 2;
-    } else if (xDiff) {
+    } else if (moveX) {
       follower[0] = leader[0];
     }
-    if (xDiff) {
+    if (moveX) {
       follower[1] = (leader[1] + follower[1]) / 2;
-    } else if (yDiff) {
+    } else if (moveY) {
       follower[1] = leader[1];
     }
   }
@@ -34,10 +34,7 @@ const follow = (leader: [number, number], follower: [number, number]) => {
 
 movements.forEach(([direction, distance]) => {
   let steps = 0;
-  // console.log(direction,distance)
   while (steps < distance) {
-    const clone = [...trail.map((a) => [...a])];
-    const headC = [...head];
     //move head
     switch (direction) {
       case "D":
@@ -64,15 +61,7 @@ movements.forEach(([direction, distance]) => {
     });
     steps++;
 
-    if (trail.join().split("").includes(".")) {
-      console.log("Before", headC, clone);
-      console.log(direction);
-      console.log(head, trail);
-      Deno.exit();
-    }
-
     visited.add(trail[trail.length - 1].join());
   }
 });
-console.log(visited);
 console.log(visited.size);
